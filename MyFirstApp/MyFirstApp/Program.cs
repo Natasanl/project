c1148073e5e89b756f05d076efe8409f1ca50f61
Natasanl <112381794+Natasanl@users.noreply.github.com>
2022-09-04 19:48:38 +0200

    finished assignment

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Vad är ditt namn? ");
+string name = Console.ReadLine();
+Console.WriteLine("Hur gammal är du? ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Did you have your breakfast?");
+Console.WriteLine("Press Y for yes");
+Console.WriteLine("Press N for no");
+   
+string answer = Console.ReadLine().ToUpper();
+Console.WriteLine($"Hello,my name is {name} and I am {age} years old.");
+bool isHungry = answer == "N";
+if (isHungry)
+{
+    Console.WriteLine("By the way I am hungry!");
+}
